8b23c1ca722ca27c20c1af1bf5395df0300dd5af
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2023-05-16 17:03:25 +0200

    wip

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import './sass/index.scss';
+import Notiflix from "notiflix"
+import { fetchImages } from "./js/fetchImages"
+import { renderGallery } from "./js/gallery";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const form = document.querySelector('.search-form')
+const gallery = document.querySelector('.gallery')
+const moreLoad = document.querySelector('.load-more')
+let page = 1
+let query = ''
+let simpleLightbox
+const perPage = 40;
+form.addEventListener('submit',handleSearch)
+moreLoad.addEventListener('click',loadButton)
+
+function handleSearch (e) {
+  
+    e.preventDefault() 
+    page = 1;
+    query = e.currentTarget.searchQuery.value.trim();
+    gallery.innerHTML='';
+moreLoad.classList.add('is-hidden');
+
+if(query === ''){
+    Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
+    return
+}
+
+fetchImages(query,page,perPage)
+    .then(({data}) => {
+        if(data.totalHits === 0){
+            Notiflix.Notify.failure(
+                'Sorry, there are no images matching your search query. Please try again.',
+              );
+        }
+else{
+    renderGallery(data.hits);
+    simpleLightbox = new SimpleLightbox('.gallery a').refresh();
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+ console.log(data)
+
+if(data.totalHits > perPage){
+    moreLoad.classList.remove('is-hidden')
+
+}
+form.reset();
+}
+    })
+.catch(error => console.log(error))
+
+
+}
+
+function loadButton () {
+page +=1;
+fetchImages(query,page,perPage).then(({data}) => {
+    renderGallery(data.hits)
+    simpleLightbox = new SimpleLightbox('.gallery a').refresh();
+    const allPages = Math.ceil(data.totalHits / perPage);
+
+    if(page > allPages){
+        moreLoad.classList.add('is-hidden')
+        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+    }
+} )
+.catch(error => console.log(error));
+
+}
+
+
+
+
+
---
+++ src/js/fetchImages.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const myKey = '36411349-fd3335cbc8c141eadb26de171'
+
+async function fetchImages(query, page, perPage) {
+    const response = await axios.get(
+      `?key=${myKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+    );
+    return response;
+  }
+
+export {fetchImages}
